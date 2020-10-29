15661c145a58de0dd22a67e6b29db925086521d5
Naveen <rajnaveen677@gmail.com>
2020-10-29 21:38:53 +0530

    commit-2

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#toggle-btn").click(function(){
+        $("div.slow").toggle("slow")
+    })
+})
+
+$(document).ready(function(){
+    $("#toggle-mens").click(function(){
+        $("div.Mens").toggle("slow")
+    })
+})
+
+
+$(document).ready(function(){
+    $("#toggle-womens").click(function(){
+        $("div.Womens").toggle("slow")
+    })
+})
